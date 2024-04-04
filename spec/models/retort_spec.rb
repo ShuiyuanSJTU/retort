651c852708939a223415b12ce81c8c4252065758
PangBo <51732678+pangbo13@users.noreply.github.com>
2024-04-04 20:53:52 +0800

    dev: spec

--- spec/models/retort_spec.rb
+++ spec/models/retort_spec.rb
@@ -1,15 +1,9 @@
-require "spec_helper"
+# frozen_string_literal: true
 
-path = "./plugins/retort/plugin.rb"
-source = File.read(path)
-plugin = Plugin::Instance.new(Plugin::Metadata.parse(source), path)
-plugin.activate!
-plugin.initializers.first.call
+require "rails_helper"
 
-describe ::Retort::Retort do
-  before do
-    SiteSetting.load_settings(File.join(Rails.root, 'plugins', 'retort', 'config', 'settings.yml'))
-  end
+describe Retort do
+  before(:example) { }
 
   let(:user) { Fabricate :user }
   let(:topic) { Fabricate :topic }
@@ -18,83 +12,77 @@ describe ::Retort::Retort do
   let(:another_topic_post) { Fabricate :post }
   let(:emoji) { 'kickbutt' }
   let(:altermoji) { 'puntrear' }
-  let(:retort) { Retort::Retort.new(post, user, emoji) }
-
+  
   describe 'initialize' do
-    it 'stores the post' do
+    let(:retort) { Retort.create(post_id: post.id, user_id: user.id, emoji: emoji) }
+
+    it 'stores the record' do
       expect(retort.post).to eq post
-    end
-    it 'stores the user' do
       expect(retort.user).to eq user
+      expect(retort.emoji).to eq emoji
     end
-    it 'stores the retort' do
-      expect(retort.retort).to eq emoji
+    it 'has timestamps' do
+      expect(retort.created_at).not_to be_nil
+      expect(retort.updated_at).not_to be_nil
+      expect(retort.updated_at).to eq retort.created_at
     end
-  end
-
-  describe 'where' do
-    let(:another_user) { Fabricate :user }
-    let(:a_third_user) { Fabricate :user }
-    let!(:existing) { Fabricate :post_detail, post: post, key: "retort_#{user.id}", value: 'retort', extra: emoji }
-    let!(:another_existing) { Fabricate :post_detail, post: post, key: "retort_#{another_user.id}", value: :retort, extra: emoji }
-    let!(:another_post_retort) { Fabricate :post_detail, post: another_post, key: "retort_#{user.id}", value: :retort, extra: emoji }
-    let!(:another_topic_retort) { Fabricate :post_detail, post: another_topic_post, key: "retort_#{user.id}", value: :retort, extra: emoji }
-
-    it 'finds an existing retort by post / user' do
-      found = Retort::Retort.where(post: post, user: user)
-      expect(found).to include existing
-      expect(found).to_not include another_existing
-    end
-
-    it 'finds existing retorts by topic / user' do
-      found = Retort::Retort.where(topic: topic)
-    end
-
-    it 'finds all retorts for a post if no user is specified' do
-      found = Retort::Retort.where(post: post)
-      expect(found).to include existing
-      expect(found).to include another_existing
-      expect(found).to_not include another_post_retort
-      expect(found).to_not include another_topic_retort
-    end
-
-    it 'finds all retorts for a topic if no user is specified' do
-      found = Retort::Retort.where(topic: topic)
-      expect(found).to include existing
-      expect(found).to include another_existing
-      expect(found).to include another_post_retort
-      expect(found).to_not include another_topic_retort
-    end
-
-    it 'returns empty when no post or topic is specified' do
-      found = Retort::Retort.where(user: user)
-      expect(found).to be_empty
-    end
-
-    it 'returns empty when no retort exists' do
-      expect(Retort::Retort.where(post: post, user: a_third_user)).to be_empty
+    it 'not deleted' do
+      expect(retort.deleted_at).to be_nil
+      expect(retort.deleted_by).to be_nil
     end
   end
 
-  describe 'save' do
-    it 'saves the retort to the posts details' do
-      expect { retort.save }.to change { PostDetail.count }.by(1)
-      expect(PostDetail.find_by(post: post, key: "retort_#{user.id}", value: :retort, extra: emoji)).to be_present
+  describe 'checks ActiveRecord valid' do
+    it 'is invalid emoji' do
+      expect{
+        Retort.create(
+          post_id: post.id, user_id: user.id, emoji: nil).save!
+        }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it 'is invalid post' do
+      invalid_post_id = post.id + 100000
+      expect{
+        Retort.create(
+          post_id: invalid_post_id, user_id: user.id, emoji: emoji).save!
+        }.to raise_error ActiveRecord::InvalidForeignKey
+    end
+    it 'is valid' do
+      expect{
+        Retort.create(
+          post_id: post.id, user_id: user.id, emoji: emoji).save!
+      }.not_to raise_error
+    end
+  end
+
+  describe 'when create, withdraw, toggle' do
+    let(:retort) { Retort.create(post_id: post.id, user_id: user.id, emoji: emoji) }
+
+    it 'can not create twice' do
+      expect(retort).not_to be_nil
+      expect{
+        Retort.create(
+          post_id: post.id, user_id: user.id, emoji: emoji).save!
+      }.to raise_error ActiveRecord::RecordNotUnique
     end
 
-    it 'overwrites an existing retort' do
-      retort.save
-      retort.retort = altermoji
-      expect { retort.save }.not_to change { PostDetail.count }
-      expect(PostDetail.find_by(post: post, key: "retort_#{user.id}", value: :retort, extra: altermoji)).to be_present
-      expect(PostDetail.find_by(post: post, key: "retort_#{user.id}", value: :retort, extra: emoji)).not_to be_present
+    it 'can withdraw' do
+      expect{
+        retort.withdraw!
+      }.to \
+        change{retort.deleted_at}.from(nil).to(be_present) \
+        .and change{retort.deleted_by}.from(nil).to(user.id)
     end
 
-    it 'deletes an empty retort' do
-      retort.save
-      retort.retort = ''
-      expect { retort.save }.to change { PostDetail.count }.by(-1)
-      expect(PostDetail.find_by(post: post, key: "retort_#{user.id}", value: :retort, extra: emoji)).not_to be_present
+    it 'can recover' do
+      original_created_at = retort.created_at
+      retort.withdraw!
+      expect{
+        retort.recover!
+      }.to \
+        change{retort.deleted_at}.from(be_present).to(nil) \
+        .and change{retort.deleted_by}.from(user.id).to(nil)
+      expect(retort.created_at).to eq original_created_at
+      expect(retort.updated_at).not_to eq original_created_at
     end
   end
 
